db28d0ba4dfe25d33c1037bc272a4303650a1e35
Takekazu Omi <tomi@baleen.studio>
2025-05-05 17:39:46 +0900

    License ヘッダーを更新

--- pkg/x/protoconv/detail.go
+++ pkg/x/protoconv/detail.go
@@ -1,6 +1,3 @@
-// Package: protoconv は、Protocol Buffersのメッセージを変換する
-package protoconv
-
 // Copyright 2025 Takekazu Omi
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,6 +11,9 @@ package protoconv
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// Package: protoconv は、Protocol Buffersのメッセージを変換する
+package protoconv
 
 import (
 	"errors"
